3346c28716850af3552c04ecfdebdb08017f0907
ShahriarSaleque <salequeshahriar@gmail.com>
2020-01-22 19:56:30 +0600

    ejs handlebars

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
+
 const port = 5000 || process.env.PORT;
+let weather_data = "";
 
 //Set EJS as template engine
 app.set("view engine", "ejs");
@@ -21,6 +23,7 @@ app.get("/", (req, res) => {
 app.post("/", async (req, res) => {
   try {
     const api_key = "b323aa23f286ee3fa739734a62bbf508";
+    let weather_data = null;
     let lower_city = req.body.city;
     let upper_city =
       lower_city.charAt(0).toUpperCase() + lower_city.substring(1);
@@ -29,9 +32,22 @@ app.post("/", async (req, res) => {
 
     //Make API call to weather forecast
     let weather = await axios(url);
-    let weather_data = weather.data;
-    console.log(weather_data);
+    weather_data = weather.data;
+
     //Render index page with data fetched from api
+    let temp = `It is ${weather_data.main.temp} Fahrenheit in ${weather_data.name}`;
+    let pressure = `Atmospheric pressure is ${weather_data.main.pressure}`;
+    let humidity = `Humidity is ${weather_data.main.humidity}`;
+
+    if (weather_data !== null) {
+      res.render("results", {
+        temp: temp,
+        pressure: pressure,
+        humidity: humidity
+      });
+    } else {
+      res.render("index");
+    }
   } catch (error) {
     res.render("error");
     console.log(error);
